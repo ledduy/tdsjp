879916594e2be3685787d37b9efe8df37907f8cd
ledduy <ledduy@users.noreply.github.com>
2018-03-26 05:30:20 +0700

    Update

--- getvideo.sh
+++ getvideo.sh
@@ -6,13 +6,30 @@ mkdir -p /home/mmlab/mbase/tdsjp/video
 
 cd /home/mmlab/mbase/tdsjp/video
 
-wget --no-check-certificate https://mbase.co.jp/tmp/20180313_01.MTS
-wget --no-check-certificate https://mbase.co.jp/tmp/20180313_02.MTS
-wget --no-check-certificate https://mbase.co.jp/tmp/20180313_03.MTS
 
-quit
+wget --no-check-certificate https://mbase.co.jp/tmp/20180321_01_iphone.mov
+wget --no-check-certificate https://mbase.co.jp/tmp/20180321_02_iphone.mov
+wget --no-check-certificate https://mbase.co.jp/tmp/20180321_03_iphone.mov
+wget --no-check-certificate https://mbase.co.jp/tmp/20180321_04_iphone.mov
+wget --no-check-certificate https://mbase.co.jp/tmp/20180321_05_iphone.mov
+wget --no-check-certificate https://mbase.co.jp/tmp/20180321_06_iphone.mov
 
-quit()
+wget --no-check-certificate https://mbase.co.jp/tmp/20180321_11_hc.mts
+wget --no-check-certificate https://mbase.co.jp/tmp/20180321_12_hc.mts
+wget --no-check-certificate https://mbase.co.jp/tmp/20180321_13_hc.mts
+
+wget --no-check-certificate https://mbase.co.jp/tmp/20180321_21_ac.mp4
+wget --no-check-certificate https://mbase.co.jp/tmp/20180321_22_ac.mp4
+
+exit '
+DONE'
+
+
+#wget --no-check-certificate https://mbase.co.jp/tmp/20180313_01.MTS
+#wget --no-check-certificate https://mbase.co.jp/tmp/20180313_02.MTS
+#wget --no-check-certificate https://mbase.co.jp/tmp/20180313_03.MTS
+
+
 
 echo DONE
 
