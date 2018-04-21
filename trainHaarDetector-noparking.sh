9f5fdae52fd077002d6db894a49e7ee32ddbd6c3
ledduy <ledduy@users.noreply.github.com>
2018-04-21 10:51:27 +0700

    start training Train2

--- trainHaarDetector-noparking.sh
+++ trainHaarDetector-noparking.sh
@@ -1,5 +1,11 @@
 # train model
 
+# Train2-ex --> bug - cropRect4NegImages - confused crop_img = img[top:bottom, left:right] and imwidth --> crop wrong rect
+# Train2-ex -- 19 rounds - still confusing with limit50
+# Training time 18 hours
+# Pos: 620
+
+# Train2 --> fix bugs in croping rect of neg-noparking, neg-noparkingx, and adding negNOT-limit50, negNOT-limit40
 TRIAL_NAME=Train2
 LABEL_NAME=noparking
 
@@ -31,9 +37,9 @@ mkdir $DETECTOR_DIR
 BG_FILE=$TRIAL_NAME/neg-noparkingx2.dat2
 
 # so luong POS phai <= so luong thuc su trong file .vec
-NUM_POS=620 #629
-NUM_NEG=620 #620
-NUM_STAGES=19 #20
+NUM_POS=620 #
+NUM_NEG=620 # = NUM_POS
+NUM_STAGES=10 # 10 - 15 - 20 - tang dan de co ket qua trung gian
 MIN_HIT_RATE=0.999
 MAX_FA_RATE=0.3
 
